6c3d88380424663e0de1dda6c5e509ec5eeed92f
Jake <vegihon@aol.com>
2014-12-16 20:14:21 -0500

    Update sv_zombiespawn.lua

--- lua/autorun/server/sv_zombiespawn.lua
+++ lua/autorun/server/sv_zombiespawn.lua
@@ -3,29 +3,44 @@ local ZombieSpawnPoints = {
     { pos = Vector( 0, 0, 0 ) }, --Add as many as you like... Get the vectors by typing "getpos" in console.
 }
 
+local numZombies = 0
+local maxZombies = 150
+
 function ZombieSpawns()
 
-     timer.Create( "Respawner", 60, 10000, function() --Respawns every 60 seconds, after spawning 10,000 zombies it will stop.
+     timer.Create( "Respawner", 10, 10, function()
 
         if ( numZombies < maxZombies ) then
 
             for k, v in pairs( ZombieSpawnPoints ) do
 
-               local zombie = ents.Create( "npc_zombie" ) --Create whatever NPC you want it to here.
-                zombie:SetPos( v.pos )
-		zombie:SetCustomCollisionCheck( true )
-                zombie:Spawn()
-                zombie:Activate()
+                    local zombie = ents.Create( "npc_zombie" )
+                    zombie:SetPos( v.pos )
+                    zombie:SetCustomCollisionCheck( true )
+                    zombie:Spawn()
+                    zombie:Activate()
 
-                numZombies = numZombies + 1
+                    numZombies = numZombies + 1
 
             end
 
         end
 
     end )
-	
+    
 end
+
+hook.Add( "ShouldCollide", "NPCNoCollide", function( ent1, ent2 )
+
+    if ( ent1:GetClass() == "npc_zombie" and ent2:GetClass() == "npc_zombie" ) then --Change the NPC to whatever you want.
+
+        return false
+
+    end
+
+end )
+
+hook.Add( "InitPostEntity", "StartZombieSpawning", ZombieSpawns )
 
 hook.Add( "ShouldCollide", "NPCNoCollide", function( ent1, ent2 )
 
